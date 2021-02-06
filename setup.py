5330d58f2d2443b595f8e13f255e4040de299c59
Ajin Abraham <ajin25@gmail.com>
2021-02-06 15:48:59 -0800

    mitmproxy bump, lgpl2.1 -> 3+

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ description = ('httptools helps you to capture, repeat'
                '[mitmproxy](https://mitmproxy.org/)')
 setup(
     name='http-tools',
-    version='2.0.0',
+    version='2.1.0',
     description=description,
     author='Ajin Abraham',
     author_email='ajin25@gmail.com',
@@ -25,8 +25,8 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        ('License :: OSI Approved :: '
-         'GNU Lesser General Public License v2 (LGPLv2)'),
+        ('License :: OSI Approved :: GNU Lesser '
+         'General Public License v3 or later (LGPLv3+)'),
         'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(include=[
@@ -43,6 +43,6 @@ setup(
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     install_requires=[
-        'mitmproxy==5.3.0',
+        'mitmproxy>=6.0.0<=6.0.2',
     ],
 )
